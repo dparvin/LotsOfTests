a9159eb566ccfad8294ddb08de79ef984348422d
David Parvin <daveparvin@cougarmtn.com>
2017-11-06 17:35:41 -0700

    Added 1,000 tests (10 classes, 10 Tests, run 10 times)

---
+++ BigTestProject/BigTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BigTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BigTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d2a6619-0ed4-48e5-9bbb-76ae70660a2a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BigTestProject/BigTestProject/UnitTest7.cs
@@ -0,0 +1,210 @@
+﻿using Xunit;
+
+namespace BigTestProject
+{
+    /// <summary>
+    /// Test Class # 1
+    /// </summary>
+    public class UnitTest7
+    {
+        /// <summary>
+        /// Tests the method1.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod1(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method2.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod2(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method3.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod3(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method4.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod4(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method5.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod5(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method6.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod6(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method7.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod7(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method8.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod8(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method9.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod9(int value)
+        {
+            Assert.True(value > 0);
+        }
+
+        /// <summary>
+        /// Tests the method10.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestMethod10(int value)
+        {
+            Assert.True(value > 0);
+        }
+    }
+}
