b79bcbf1988276f30a1c85ac6f34948ffff10f83
David Parvin <daveparvin@cougarmtn.com>
2017-12-15 16:45:31 -0700

    Added Test project with a trait that has no value part to it.

---
+++ BigTestProject/TraitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuidTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuidTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80595067-a293-4d23-bc8c-dff84783a48d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BigTestProject/TraitTestProject/TraitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace BigTestProject
+{
+    /// <summary>
+    /// Guid Tests
+    /// </summary>
+    public class TraitTests
+    {
+        /// <summary>
+        ///A test with a trait
+        ///</summary>
+        [Fact]
+        [Trait("UnitTest", "")]
+        public void Test()
+        {
+        }
+
+    }
+}
